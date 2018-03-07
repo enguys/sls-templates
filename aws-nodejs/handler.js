cbe9fd86538a2152b3c0f87b1a44ef224c00c379
Minh Tran <minhtranite@gmail.com>
2018-03-07 15:20:16 +0700

    Update

--- aws-nodejs/handler.js
+++ aws-nodejs/handler.js
@@ -1,10 +1,13 @@
-import { createResponse } from './ultis/http';
-
 export const hello = (event, context, callback) => {
-  callback(null, createResponse({
-    message: 'Go Serverless v1.0! Your function executed successfully!',
-    input: event,
-  }));
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
--- aws-nodejs/ultis/logger.js
+++
@@ -1,7 +0,0 @@
-const debug = () => {
-  if (process.env.DEBUG) {
-    console.log(...arguments);
-  }
-};
-
-export { debug };
