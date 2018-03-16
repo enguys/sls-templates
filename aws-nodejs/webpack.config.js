781cf7289d2f0292914295a7d1447f9b1a8ee822
Minh Tran <minhtranite@gmail.com>
2018-03-16 14:07:50 +0700

    Update webpack@4

--- aws-nodejs/webpack.config.js
+++ aws-nodejs/webpack.config.js
@@ -5,6 +5,7 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
